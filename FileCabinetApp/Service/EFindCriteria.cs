21ceac22317db37a3a76304fb8d95881963f29ed
Stanislav Zaycev <stas315172004@mail.ru>
2023-06-25 19:15:53 +0300

    Merge step4-add-search branch: Added Find command option. Added EFindCriteria Enum.

---
+++ FileCabinetApp/Service/EFindCriteria.cs
@@ -0,0 +1,42 @@
+﻿namespace FileCabinetApp.Service;
+
+/// <summary>
+/// Criteria used in Find meethod in FileCabinetSetvise.
+/// </summary>
+public enum EFindCriteria
+{
+    /// <summary>
+    /// Searching by first name.
+    /// </summary>
+    FirstName,
+
+    /// <summary>
+    /// Searching by last name.
+    /// </summary>
+    LastName,
+
+    /// <summary>
+    /// Searching by age.
+    /// </summary>
+    Sex,
+
+    /// <summary>
+    /// Searching by date of birth.
+    /// </summary>
+    DateOfBirth,
+
+    /// <summary>
+    /// Searching by post index.
+    /// </summary>
+    PostIndex,
+
+    /// <summary>
+    /// Searching by id.
+    /// </summary>
+    Id,
+
+    /// <summary>
+    /// Searching by country.
+    /// </summary>
+    Country,
+}
